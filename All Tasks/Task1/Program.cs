71df52abff211c1d985f8dbb73de147fd818d47a
Александр Донченко <darkprince3738@gmail.com>
2021-06-20 00:33:30 +0300

    Maybe done

---
+++ All Tasks/Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var User = new User();
+            Console.WriteLine("Write your first name");
+            try
+            {
+                User.FirstName = Console.ReadLine();
+            }
+            catch (FocusPocusException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            Console.WriteLine("Write your last name");
+            User.LastName = Console.ReadLine();
+            Console.WriteLine("Write your middle name");
+            User.MiddleName = Console.ReadLine();
+            Console.WriteLine("Write your age");
+            User.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Write your gender");
+            User.Gender = (Gender) Enum.Parse(typeof(Gender), Console.ReadLine());
+            Console.WriteLine("Write your job");
+            User.Job = Console.ReadLine();
+            Console.WriteLine("Write your education");
+            User.Education = Console.ReadLine();
+            Console.WriteLine("Write your married status");
+            User.MaritalStatus = (MarriedStatus) Enum.Parse(typeof(MarriedStatus), Console.ReadLine());
+            Console.WriteLine($"{User.FirstName}, {User.LastName}, {User.MiddleName}, {User.Age}, {User.Gender}, {User.Job}, {User.Education}, {User.MaritalStatus}");
+        }
+    }
+    public class FocusPocusException : Exception
+    {
+        public FocusPocusException() { }
+        public FocusPocusException(string message): base(message)
+        {
+
+        }
+        public FocusPocusException(string message, Exception innerException): base(message, innerException)
+        {
+
+        }
+    }
+}
---
+++ All Tasks/Task1/User.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class User
+    {
+        private string firstName;
+        private string lastName;
+        private string middleName;
+        private int age;
+        private Gender gender;
+        private string job;
+        private string education;
+        private MarriedStatus maritalStatus;
+
+        public string FirstName
+        {
+            set
+            {
+                if (value == "" || value == null)
+                {
+                    throw new Exception("First Name is empty");
+                }
+                else if(value.Length > 100 && value[0] != value.ToUpper()[0])
+                {
+
+                    throw new Exception("First letter must start with a capital letter");
+                }
+                firstName = value;
+            }
+            get
+            {
+                return firstName;
+            }
+        }
+        public string LastName
+        {
+            set
+            {
+                if (value == "" || value == null)
+                {
+                    throw new Exception("Last Name is empty");
+                }
+                else if (value.Length > 100 && value[0] != value.ToUpper()[0])
+                {
+
+                    throw new Exception("First letter must start with a capital letter");
+                }
+                lastName = value;
+            }
+            get
+            {
+                return lastName;
+            }
+        }
+        public string MiddleName
+        {
+            set
+            {
+                if (value == "" || value == null)
+                {
+                    throw new Exception("Middle Name is empty");
+                }
+                else if (value.Length > 100 && value[0] != value.ToUpper()[0])
+                {
+
+                    throw new Exception("First letter must start with a capital letter");
+                }
+                middleName = value;
+            }
+            get
+            {
+                return middleName;
+            }
+        }
+        public int Age
+        {
+            set
+            {
+                if (age<0 && age >120)
+                {
+                    throw new Exception("Age is invalid. Age has value between 0 and 120");
+                }
+                age = value;
+            }
+            get
+            {
+                return age;
+            }
+        }
+        public Gender Gender
+        {
+            set
+            {
+                if (value != Gender.Male && value != Gender.Female)
+                {
+                    throw new Exception("Gender is required");
+                }
+                gender = value;
+            }
+            get
+            {
+                return gender;
+            }
+        }
+        public string Job
+        {
+            set
+            {
+                if (value == "" || value == null)
+                {
+                    throw new Exception("Job is required");
+                }
+                else if (value.Length > 300)
+                {
+                    throw new Exception("Job length must be less than 300");
+                }
+                job = value;
+            }
+            get
+            {
+                return job;
+            }
+        }
+        public string Education
+        {
+            set
+            {
+                if (value == "" || value == null)
+                {
+                    throw new Exception("Job is required");
+                }
+                else if (value.Length > 300)
+                {
+                    throw new Exception("Education length must be less than 300");
+                }
+                education = value;
+            }
+            get
+            {
+                return education;
+            }
+        }
+        public MarriedStatus MaritalStatus
+        {
+            set
+            {
+                if (value != MarriedStatus.Married && value != MarriedStatus.Divorced && value != MarriedStatus.NotMarried)
+                {
+                    throw new Exception("MaritalStatus is required");
+                }
+                maritalStatus = value;
+            }
+            get
+            {
+                return maritalStatus;
+            }
+        }
+    }
+    enum Gender
+    {
+        Male,
+        Female
+    }
+    enum MarriedStatus
+    {
+        Married,
+        Divorced,
+        NotMarried
+    }
+}
